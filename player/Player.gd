56b99170937809cc36c385ad34605229422c09eb
Quinn Chrest <quinnchr@gmail.com>
2024-06-07 22:59:50 -0500

    got player working with tile map collisions

--- player/Player.gd
+++ player/Player.gd
@@ -1,5 +1,9 @@
-extends Area2D
+extends CharacterBody2D
 signal hit
+
+const GRAVITY = 400.0
+const WALK_SPEED = 100
+const VERTICAL = -200
 
 @export var speed = 400 # How fast the player will move (pixels/sec).
 var screen_size # Size of the game window.
@@ -7,36 +11,38 @@ var screen_size # Size of the game window.
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	screen_size = get_viewport_rect().size
+	$AnimatedSprite2D.play()
 	hide()
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	var velocity = Vector2.ZERO # The player's movement vector.
+	velocity.y += delta * GRAVITY
+	
 	if Input.is_action_pressed("move_right"):
-		velocity.x += 1
-	if Input.is_action_pressed("move_left"):
-		velocity.x -= 1
-
-	if velocity.length() > 0:
-		velocity = velocity.normalized() * speed
-		$AnimatedSprite2D.play()
+		velocity.x = WALK_SPEED
+	elif Input.is_action_pressed("move_left"):
+		velocity.x = -WALK_SPEED
 	else:
-		$AnimatedSprite2D.stop()
-		
-	position += velocity * delta
-	position = position.clamp(Vector2.ZERO, screen_size)
+		velocity.x = 0
+	
+	if is_on_floor() and Input.is_action_just_pressed("jump"):
+		velocity.y = VERTICAL
+	
+	move_and_slide()
 	
 	if velocity.x != 0:
 		$AnimatedSprite2D.animation = "Walk"
 		$AnimatedSprite2D.flip_h = velocity.x < 0
 	else:
 		$AnimatedSprite2D.animation = "Idle"
-
+		
+	
 
 func _on_body_entered(body):
 	hide()
 	hit.emit()
+	$AnimatedSprite2D.stop()
 	$CollisionShape2D.set_deferred("disabled", true)
 	
 	
