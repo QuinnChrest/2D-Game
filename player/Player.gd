106acbcb00715c32d1e12f2617c63fdff3af7a55
Quinn Chrest <quinnchr@gmail.com>
2024-06-19 13:27:44 -0500

    attack animation working

--- player/Player.gd
+++ player/Player.gd
@@ -7,12 +7,13 @@ const VERTICAL = -325
 
 var screen_size # Size of the game window.
 
+var attack_complete = true
+
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	screen_size = get_viewport_rect().size
 	$AnimatedSprite2D.play()
 	hide()
-
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
@@ -30,22 +31,31 @@ func _process(delta):
 	
 	move_and_slide()
 	
-	if velocity.x != 0:
-		$AnimatedSprite2D.animation = "Walk"
-		$AnimatedSprite2D.flip_h = velocity.x < 0
-	else:
-		$AnimatedSprite2D.animation = "Idle"
-		
+	if Input.is_action_just_pressed("attack") && attack_complete:
+		$AnimatedSprite2D.animation = "Attack"
+		attack_complete = false
+	elif attack_complete:
+		if velocity.x != 0:
+			$AnimatedSprite2D.animation = "Walk"
+			$AnimatedSprite2D.flip_h = velocity.x < 0
+		else:
+			$AnimatedSprite2D.animation = "Idle"
 	
+	$AnimatedSprite2D.play()
 
 func _on_body_entered(body):
 	hide()
 	hit.emit()
 	$AnimatedSprite2D.stop()
 	$CollisionShape2D.set_deferred("disabled", true)
-	
-	
+
 func start(pos):
 	position = pos
 	show()
 	$CollisionShape2D.disabled = false
+	
+
+func _on_animated_sprite_2d_animation_finished():
+	if($AnimatedSprite2D.animation == "Attack"):
+		attack_complete = true
+		$AnimatedSprite2D.animation = "Idle"
